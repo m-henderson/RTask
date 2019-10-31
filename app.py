b9c37f8067db000ce64533c0f701598f90385355
Michael Henderson <michael.v.henderson@icloud.com>
2019-10-31 06:56:51 -0500

    im flask

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, Response
+from flask_login import LoginManager
+from models.user import User
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Our mock database
+users = {'mvhenderson.4@gmail.com': {'password': 'secret'}}
 
 @app.route('/')
 def hello():
     name = 'michael'
     return render_template('home.html', name=name)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if flask.request.method == 'GET':
+        return render_template('login.html')
+
+    email = flask.request.form['email']
+    if flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+    
+    return 'Bad login'
+    
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
---
+++ models/user.py
@@ -0,0 +1,26 @@
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return 
+
+    user = User()
+    user.id = email
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user

ca25e547adee2df1935899b8783a19c01253e278
Michael Henderson <michael.v.henderson@icloud.com>
2019-11-09 07:25:49 -0600

    restructure

--- app/models/ticket.py
+++ app/models/ticket.py
@@ -8,7 +8,6 @@ class Ticket(Base):
     id = sa.Column(sa.BigInteger, autoincrement=True, primary_key=True)
     title = sa.Column(sa.Unicode(100), nullable=False)
     description = sa.Column(sa.Unicode(255), nullable=False)
-    created_date = sa.Column(sa.DateTime, default=datetime.datetime.utcnow)
 
    
 

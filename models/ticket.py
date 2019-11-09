f34adbb227dce0341fc52b01d5d4a369defea805
Michael Henderson <michael.v.henderson@icloud.com>
2019-11-09 05:21:24 -0600

    add created date prop to ticket

--- models/ticket.py
+++ models/ticket.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sa
+import datetime
 from db import Base
 
 class Ticket(Base):
@@ -7,6 +8,7 @@ class Ticket(Base):
     id = sa.Column(sa.BigInteger, autoincrement=True, primary_key=True)
     title = sa.Column(sa.Unicode(100), nullable=False)
     description = sa.Column(sa.Unicode(255), nullable=False)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.utcnow)
 
    
 
